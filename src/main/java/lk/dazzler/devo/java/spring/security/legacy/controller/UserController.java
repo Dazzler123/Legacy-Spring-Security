cef6d984b27a734d8df95cac91a723391fc31d8f
Dasindu Hewagamage <dasinduhewagamage@gmail.com>
2023-08-02 12:25:23 +0530

    Introduced UserController

---
+++ src/main/java/lk/dazzler/devo/java/spring/security/legacy/controller/UserController.java
@@ -0,0 +1,10 @@
+/**
+ * #=== Created by Dazzler7Soft.Eng.Udg ===#
+ * Date : 02-Aug-2023
+ * Time : 12:25 PM
+ * Project Name : Spring-Security-Legacy
+ */
+package lk.dazzler.devo.java.spring.security.legacy.controller;
+
+public class UserController {
+}
