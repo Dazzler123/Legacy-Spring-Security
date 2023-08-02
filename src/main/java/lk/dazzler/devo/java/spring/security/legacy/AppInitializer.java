1c345b8dcc85cd25a6e2a1f4f8ba88447dc48e0a
Dasindu Hewagamage <dasinduhewagamage@gmail.com>
2023-08-02 11:50:49 +0530

    Content updated

--- src/main/java/lk/dazzler/devo/java/spring/security/legacy/AppInitializer.java
+++
@@ -1,7 +0,0 @@
-package lk.dazzler.devo.java.spring.security.legacy;
-
-public class AppInitializer {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
-}
