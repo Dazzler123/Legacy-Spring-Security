cdc3e692fd39e1562a55db5a693b06592b67b156
Dasindu Hewagamage <dasinduhewagamage@gmail.com>
2023-08-02 10:44:16 +0530

    Dependencies added

--- src/main/java/lk/dazzler/devo/java/spring/security/legacy/AppInitializer.java
+++ src/main/java/lk/dazzler/devo/java/spring/security/legacy/AppInitializer.java
@@ -1,6 +1,6 @@
 package lk.dazzler.devo.java.spring.security.legacy;
 
-public class Main {
+public class AppInitializer {
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
