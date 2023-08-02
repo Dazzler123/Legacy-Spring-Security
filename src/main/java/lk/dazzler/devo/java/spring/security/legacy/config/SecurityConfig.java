0df4cd12b193315d63d54d29a10258cd302ca247
Dasindu Hewagamage <dasinduhewagamage@gmail.com>
2023-08-02 12:24:45 +0530

    Content added

--- src/main/java/lk/dazzler/devo/java/spring/security/legacy/config/SecurityConfig.java
+++ src/main/java/lk/dazzler/devo/java/spring/security/legacy/config/SecurityConfig.java
@@ -6,5 +6,27 @@
  */
 package lk.dazzler.devo.java.spring.security.legacy.config;
 
-public class SecurityConfig {
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception{
+        auth.inMemoryAuthentication().
+                withUser("Dazzler").
+                password("dazzler7@Tec").
+                roles("Admin");
+    }
+
+    @Bean
+    public PasswordEncoder getPasswordEncoder(){
+        return NoOpPasswordEncoder.getInstance();
+    }
+
 }
