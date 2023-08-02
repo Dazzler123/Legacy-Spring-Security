80408e552bf3a901b7333bd5f50277415684449e
Dasindu Hewagamage <dasinduhewagamage@gmail.com>
2023-08-02 12:05:01 +0530

    Introduced SecurityConfig

---
+++ src/main/java/lk/dazzler/devo/java/spring/security/legacy/config/SecurityConfig.java
@@ -0,0 +1,10 @@
+/**
+ * #=== Created by Dazzler7Soft.Eng.Udg ===#
+ * Date : 02-Aug-2023
+ * Time : 12:04 PM
+ * Project Name : Spring-Security-Legacy
+ */
+package lk.dazzler.devo.java.spring.security.legacy.config;
+
+public class SecurityConfig {
+}
